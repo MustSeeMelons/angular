e7a1b72a56264b28e2df98dfe40e83170c6ca13e
Janis Strautins <strautins.janis@gmail.com>
2020-12-28 16:13:40 +0200

    Some werk

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  name = 'Max';
+  name = "Max";
+  rand = (Math.random() * 10000).toFixed(0);
 }
