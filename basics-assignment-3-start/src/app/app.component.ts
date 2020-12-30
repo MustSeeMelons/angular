6984ae40897daac6c8281486a1a5298d23418af7
Janis Strautins <strautins.janis@gmail.com>
2020-12-30 13:11:15 +0200

    Assingment 3 done

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  styles: [
+    `
+      .fromFive {
+        color: white;
+      }
+    `,
+  ],
 })
 export class AppComponent {
+  isVisible = true;
+  clickHistory = [];
+  counter = 0;
+
+  onClick = () => {
+    this.isVisible = !this.isVisible;
+    this.clickHistory.push(++this.counter);
+  };
+
+  getBackground = (index: number) => {
+    return index >= 4 ? "blue" : "inherit";
+  };
 }
