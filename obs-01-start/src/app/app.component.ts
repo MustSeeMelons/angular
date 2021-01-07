aa040fe32e665f5ae476697280d5bf95adba822b
Janis Strautins <strautins.janis@gmail.com>
2021-01-07 19:02:28 +0200

    Observables done

--- obs-01-start/src/app/app.component.ts
+++ obs-01-start/src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { UserService } from "./user.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  userActivated = false;
+  sub: Subscription;
+  constructor(private userService: UserService) {}
 
-  ngOnInit() {}
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  ngOnInit() {
+    this.sub = this.userService.activatedEmitter.subscribe((value: boolean) => {
+      this.userActivated = value;
+    });
+  }
 }
--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Observable, Subscription } from "rxjs";
+import { map, filter } from "rxjs/operators";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private intervalSubscription: Subscription;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    // this.intervalSubscription = interval(1000).subscribe((num) => {
+    //   console.log(num);
+    // });
+
+    const customInterval = new Observable((observer) => {
+      let counter = 0;
+      setInterval(() => {
+        observer.next(counter++);
+
+        if (counter > 100) {
+          observer.complete();
+        }
+
+        if (counter > 101) {
+          observer.error(new Error("Couner > 3"));
+        }
+      }, 1000);
+    });
+
+    this.intervalSubscription = customInterval
+      .pipe(
+        filter((data: number) => {
+          return data % 2 === 0;
+        }),
+        map((data: number) => {
+          return `Round: ${data}`;
+        })
+      )
+      .subscribe(
+        (num) => {
+          console.log(num);
+        },
+        (error) => {
+          console.log(error);
+          alert(error);
+        },
+        () => {
+          alert("Counter Done.");
+        }
+      );
   }
 
+  ngOnDestroy(): void {
+    this.intervalSubscription.unsubscribe();
+  }
 }
